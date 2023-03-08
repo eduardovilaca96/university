6f01892128a80640849593b774c9e7c57c5f077c
eduardovilaca96 <eduvilaca532@gmail.com>
2023-03-08 19:33:42 +0000

    mini_testePI

---
+++ MT_PI/a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i,k;
+    for(i = 2; i < 41; i *=2){
+        printf("1");
+    } // runs 5 times
+
+    for(k = 3; k < 20;k+=3 ){
+        printf("1");
+    }// the condition to loop 6 times is k+=3
+
+}
